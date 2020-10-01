ccd7d814807cc964575b83009d227e7d7fa0edd2
borislav nikolov <borislav@fastmail.com>
2020-10-01 19:39:59 +0200

    lock doesnt help here

--- mem.go
+++ mem.go
@@ -198,13 +198,14 @@ func (m *MemOnlyIndex) NewTermQuery(field string, term string) iq.Query {
 //  	log.Printf("%v matching with score %f", city, score)
 //  })
 func (m *MemOnlyIndex) Foreach(query iq.Query, cb func(int32, float32, Document)) {
-	m.RLock()
-	defer m.RUnlock()
-
 	for query.Next() != iq.NO_MORE {
 		did := query.GetDocId()
 		score := query.Score()
 		doc := m.forward[did]
+		if doc == nil {
+			// deleted
+			continue
+		}
 		cb(did, score, doc)
 	}
 }
